6e0218f478004ed900c11a1f2192ac8869131961
Mark Thompson <mark.thompson@smpl.company>
2018-02-11 18:16:36 -0800

    debug option

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -19,21 +19,35 @@ export class HomePage {
 
     showIFrame: boolean;
 
+    doDebug: boolean;
+
     constructor(public navCtrl: NavController, private iab: InAppBrowser) {
       this.JSON = JSON;
+      this.loadstopEvents = [];
+  	}
 
-  		this.options = '';
+    toggleDebug() {
+      this.doDebug = !this.doDebug;
+    }
+
+  	moo() {
+  		const url = 'https://smpl-talk-develop.firebaseapp.com/#/';
+  		const target = '_blank';
+
+      this.options = '';
 
       var optionAry = [];
 
       optionAry.push("disallowoverscroll=yes");//(iOS) Turns on/off the UIWebViewBounce property.
       optionAry.push("keyboardDisplayRequiresUserAction=no");// (iOS) Should take care of ios not allowing focus on inputs
       // optionAry.push("hidden=yes");
-
-      optionAry.push("toolbar=yes");// (iOS) Should be testing only
-      optionAry.push("location=yes"); // Should be testing only
-      // optionAry.push("clearcache=yes");// Should be testing only
-      // optionAry.push("clearsessioncache=yes");// Should be testing only
+      if (this.doDebug) {
+        optionAry.push("toolbar=yes");// (iOS) Should be testing only
+        optionAry.push("location=yes"); // Should be testing only
+        optionAry.push("clearcache=yes");// Should be testing only
+        optionAry.push("clearsessioncache=yes");// Should be testing only
+      }
+      
 
       for (var i = 0; i < optionAry.length; i++) {
         this.options += optionAry[i];
@@ -41,15 +55,6 @@ export class HomePage {
          this.options += ",";
         }
       }
-
-      this.cow = 'cow';
-
-      this.loadstopEvents = [];
-  	}
-
-  	moo() {
-  		const url = 'https://smpl-talk-develop.firebaseapp.com/#/';
-  		const target = '_blank';
 
   		this.browser = this.iab.create(url, target, this.options);
 
