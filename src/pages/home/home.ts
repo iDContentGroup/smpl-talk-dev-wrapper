92f4ff9d82292d1c677c311823253b09713903cc
Mark Thompson <mark.thompson@smpl.company>
2018-02-20 19:06:58 -0800

    ios test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -80,8 +80,8 @@ export class HomePage {
           });
 
           this.browser.on("loadstop").subscribe(event => {
-            this.browser.executeScript({ code: "localStorage.setItem('nativeAppMode', 'moo');" });
-            this.browser.executeScript({ code: 'window.my.activateAppMode.publicFunc();'});
+            // this.browser.executeScript({ code: "localStorage.setItem('nativeAppMode', 'moo');" });
+            // this.browser.executeScript({ code: 'window.my.activateAppMode.publicFunc();'});
 
             this.browser.executeScript({
               code: "localStorage.setItem('nativeAppTime', '" + 101 + "');"
