0a278f37ee7064eecb04ed881b2c840523207f5e
Mark Thompson <mark.thompson@smpl.company>
2018-05-09 10:59:27 -0700

    scripts instead of hooks

---
+++ scripts/patch-android-studio-check.js
@@ -0,0 +1,20 @@
+/**
+ * sources:
+ *	https://gist.github.com/joeljeske/68121fa6d643e0937f50458d0172e16e
+ *	https://stackoverflow.com/questions/48006196/ionic-build-androids-error-copyfilesync-could-not-write-to-dest-file/49770295#49770295
+ *
+ * This hook overrides a function check at runtime. Currently, cordova-android 7+ incorrectly detects that we are using
+ * an eclipse style project. This causes a lot of plugins to fail at install time due to paths actually being setup
+ * for an Android Studio project. Some plugins choose to install things into 'platforms/android/libs' which makes
+ * this original function assume it is an ecplise project.
+ */
+module.exports = function(context) {
+  if (context.opts.cordova.platforms.indexOf('android') < 0) {
+    return;
+  }
+
+  const path = context.requireCordovaModule('path');
+  const androidStudioPath = path.join(context.opts.projectRoot, 'platforms/android/cordova/lib/AndroidStudio');
+  const androidStudio = context.requireCordovaModule(androidStudioPath);
+  androidStudio.isAndroidStudioProject = function() {  return true; };
+};
