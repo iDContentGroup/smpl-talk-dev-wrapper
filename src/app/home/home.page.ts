17903ca358e796f28dccb8827857079577b6caee
= <moomoomamoo@gmail.com>
2019-02-26 11:53:55 -0800

    imported inappbrowser

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    constructor(private iab: InAppBrowser) {
+    }
 
+    test() {
+        // const browser = this.iab.create('https://ionicframework.com/');
+        var ref = this.iab.create('https://google.com', 'system', 'location=yes');
+    }
 }
