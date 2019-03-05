9cfd3921975d624eda586f354b57b76d6576417d
= <moomoomamoo@gmail.com>
2019-03-05 13:44:19 -0800

    red test 3

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -132,7 +132,7 @@ export class HomePage {
 
 
         this.doDebug = false;//true;
-        this.networkColor = '#d91515';
+        this.networkColor = 'red';
         this.rootUrl = 'https://smpltalkdev.com/#/';
         this.browserUrl = this.rootUrl;
 
